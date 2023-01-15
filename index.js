cce0d5c05e3a0a43388f387d0f5a0fe4dfcf458d
Matthew <turbodieseldodge@gmail.com>
2023-01-14 19:48:29 -0600

    clean up code

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// document.addEventListener('keydown', function (event) {
-//     if (event.key === 'ArrowLeft') {
-//         const leftNumbers = dodger.style.left.replace('px', '')
-//         const left = parseInt(leftNumbers, 10)
-
-//         dodger.style.left = `${left - 1}px`
-//     }
-// })
-
 const dodger = document.getElementById('dodger')
 
 function moveDodgerLeft() {
